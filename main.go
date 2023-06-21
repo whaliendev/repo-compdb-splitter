f54292b7fd86ace4277d63c97a8c19c952e4314e
Hwa <hwahe.cs@gmail.com>
2023-06-21 20:10:39 +0800

    build: add sonic dep

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"fmt"
+	"github.com/bytedance/sonic"
+)
 
+func main() {
+	var o1 = map[string]interface{}{
+		"a": "b",
+	}
+	var o2 = 1
+	var w = bytes.NewBuffer(nil)
+	var enc = sonic.ConfigDefault.NewEncoder(w)
+	err := enc.Encode(o1)
+	if err != nil {
+		return
+	}
+	err = enc.Encode(o2)
+	if err != nil {
+		return
+	}
+	fmt.Println(w.String())
 }
